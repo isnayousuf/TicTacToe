348c5ae26a5bbd36116d21a113d1b099ca1fddff
Isna Yousuf <isnayousuf77@gmail.com>
2021-12-06 17:47:17 +0500

    Chnaging turns  and checking whowon the game

--- game.js
+++ game.js
@@ -25,3 +25,47 @@ Array.from(boxes).forEach(element => {
         }
      });
 });
+
+
+const checkWin = () => {
+    //check logic of wining by knowing position of x and 0 
+   let boxtext = document.getElementsByClassName('boxtext')
+     let wins = [
+         [0,1,2], 
+         [3,4,5], 
+         [6,7,8],
+         [0,4,8],
+         [2,4,6],
+         [0,3,6],
+         [1,4,7],
+         [2,5,8]
+    ]
+   wins.forEach ( e => {
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && 
+        (boxtext[e[2]].innerText === boxtext[ e[1]].innerText) &&
+        (boxtext[ e[0]].innerText !== '') ){
+            document.querySelector('.info').innerText = boxtext[ e[0]].innerText + " has won the game " ;
+            isgameover = true;
+            gameover.play();
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "150px"
+        }
+   })
+}
+
+//Main logic to work the game
+ 
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+     let boxtext = element.querySelector(' .boxtext ');
+     element.addEventListener('click', ()=> {
+         if(boxtext.innerText === '') {
+             boxtext.innerText = turn;
+             turn = changeTurn();   //to get turn of each
+             audioTurn.play();
+             checkWin();
+             if(!isgameover) {
+             document.getElementsByClassName("info")[0].innerText = "Turn for " +turn;
+         }
+        }
+     });
+});
